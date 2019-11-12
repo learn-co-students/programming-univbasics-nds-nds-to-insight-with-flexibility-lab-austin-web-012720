53d4b1dfa4fde34d96dd248f477dd4387cea04ae
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-12 12:38:42 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,6 +31,8 @@ def movie_with_director_name(director_name, movie_data)
 end
 
 
+
+
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
@@ -48,8 +50,19 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-end
+  
+ director_movie = []
+  index = 0
 
+  while index < movies_collection.length do
+    movie_info = movies_collection[index]
+    director_movie << movie_with_director_name(name, movie_info)
+    index += 1
+  end
+
+  director_movie
+end
+#pp movies_with_director_key('Steven Spielberg', directors_database)
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
@@ -57,13 +70,36 @@ def gross_per_studio(collection)
   # each studio.
   #
   # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
+  # * collection: Array of Hashes where each Hash represents a movie
   #
   # RETURN:
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  
+  
+  result = {}
+  index = 0 
+  
+  while index<collection.length do 
+    movies = collection[index]
+    
+    if !result[movies[:studio]]
+      result[movies[:studio]]= movies[:worldwide_gross]
+      
+    else
+      result[movies[:studio]] += movies[:worldwide_gross]
+    end
+      index +=1 
+    end
+      result
 end
+
+
+
+
+
+
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
@@ -76,7 +112,26 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  index = 0
+  a_o_a_movies_by_dir = []
+
+  while index < source.length do
+    dir_info_hash = source[index]
+    director_name = dir_info_hash[:name]
+    directors_movies = dir_info_hash[:movies]
+    a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
+    index += 1
+  end
+
+  a_o_a_movies_by_dir
 end
+
+#pp movies_with_directors_set(directors_database)
+
+
+
+
+
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
@@ -87,3 +142,6 @@ def studios_totals(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
 end
+#pp directors_database
+#pp  studios_totals(directors_database)
+pp directors_database
